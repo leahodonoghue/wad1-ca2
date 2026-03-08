efe90a5082f13e2c7ab57ccff46e4f814ac91062
Leah O'Donoghue <leahodonoghue@Leahs-MacBook-Air-3.local>
2026-03-08 14:27:22 +0000

    added appstore link to abous js

--- controllers/about.js
+++ controllers/about.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import logger from "../utils/logger.js";
+import appStore from "../models/app-store.js";
 
 const about = {
   createView(request, response) {
@@ -8,6 +9,7 @@ const about = {
     
     const viewData = {
       title: "About the Travel Destination App",
+      info: appStore.getAppInfo()
     };
     
     response.render('about', viewData);   
